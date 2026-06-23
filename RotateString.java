37b0e4cc538488d14dc1a5201a78eb08bf81de52
chetanpathak0209-cloud <chetanpathak0209@gmail.com>
2026-06-23 23:33:26 +0530

    Add files via upload

---
+++ RotateString.java
@@ -0,0 +1,18 @@
+public class RotateString {
+
+    public static boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        String doubled = s + s;
+        return doubled.contains(goal);
+    }
+
+    public static void main(String[] args) {
+        String s = "abcde";
+        String goal = "cdeab";
+
+        System.out.println(rotateString(s, goal));
+    }
+}
