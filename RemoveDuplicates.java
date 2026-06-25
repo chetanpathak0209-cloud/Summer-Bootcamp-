2b02134cf893539e1c44865012dc7b5022d8ee48
chetanpathak0209-cloud <chetanpathak0209@gmail.com>
2026-06-25 22:45:02 +0530

    Add files via upload

---
+++ RemoveDuplicates.java
@@ -0,0 +1,48 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class RemoveDuplicates {
+
+    static Node removeDuplicates(Node head) {
+        Node current = head;
+
+        while (current != null && current.next != null) {
+            if (current.data == current.next.data) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+    static void display(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(1);
+        head.next = new Node(1);
+        head.next.next = new Node(2);
+        head.next.next.next = new Node(3);
+        head.next.next.next.next = new Node(3);
+        head.next.next.next.next.next = new Node(4);
+
+        head = removeDuplicates(head);
+
+        System.out.print("Linked List after removing duplicates: ");
+        display(head);
+    }
+}
